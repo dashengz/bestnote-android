640b200b1173005c4ce057931123d90c1fee4bf2
dashengz <contact@dashengz.com>
2017-05-31 23:54:49 -0400

    Improved Login Activity

--- app/src/main/java/com/dashengz/bestnote/LoginActivity.java
+++ app/src/main/java/com/dashengz/bestnote/LoginActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -33,15 +35,12 @@ public class LoginActivity extends AppCompatActivity {
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
-                    // User is signed in
                     Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
                     startActivity(new Intent(LoginActivity.this, MainActivity.class));
                     finish();
                 } else {
-                    // User is signed out
                     Log.d(TAG, "onAuthStateChanged:signed_out");
                 }
-                // ...
             }
         };
 
@@ -64,40 +63,45 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void register(View view) {
+        String email = vEmail.getText().toString();
+        String password = vPassword.getText().toString();
+        if (validate(vEmail, email) && validate(vPassword, password))
         mAuth.createUserWithEmailAndPassword(vEmail.getText().toString(), vPassword.getText().toString())
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
-
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
                         if (!task.isSuccessful()) {
                             Log.w(TAG, "createUserWithEmail:failed", task.getException());
+                            Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
-
-                        // ...
                     }
                 });
     }
 
     public void signIn(View view) {
-        mAuth.signInWithEmailAndPassword(vEmail.getText().toString(), vPassword.getText().toString())
+        String email = vEmail.getText().toString();
+        String password = vPassword.getText().toString();
+        if (validate(vEmail, email) && validate(vPassword, password))
+            mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
-
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
                         if (!task.isSuccessful()) {
                             Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
-
-                        // ...
                     }
                 });
+    }
+
+    private boolean validate(EditText field, String input) {
+        boolean valid = true;
+        if (TextUtils.isEmpty(input)) {
+            field.setError("Required Field!");
+            valid = false;
+        } else field.setError(null);
+        return valid;
     }
 }
