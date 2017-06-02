8253a34f810bb9586f69da1ab4ee624a145203d4
dashengz <contact@dashengz.com>
2017-06-01 23:23:03 -0400

    Improved auth and user

--- app/src/main/java/com/dashengz/bestnote/NotesActivity.java
+++ app/src/main/java/com/dashengz/bestnote/NotesActivity.java
@@ -1,11 +1,9 @@
 package com.dashengz.bestnote;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -31,7 +29,6 @@ public class NotesActivity extends AppCompatActivity {
     private RecyclerView.LayoutManager mLayoutManager;
 
     private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
     private FirebaseUser user;
 
     @Override
@@ -44,46 +41,6 @@ public class NotesActivity extends AppCompatActivity {
 
         mDatabase = FirebaseDatabase.getInstance();
         mAuth = FirebaseAuth.getInstance();
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-                    userRef = mDatabase.getReference(user.getUid());
-                    userRef.child("notes").addChildEventListener(new ChildEventListener() {
-                        @Override
-                        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                            Note note = dataSnapshot.getValue(Note.class);
-                            String key = dataSnapshot.getKey();
-                            if (!keys.contains(key)) {
-                                keys.add(key);
-                                notes.add(note);
-                                mAdapter.notifyItemInserted(notes.size() - 1);
-                            }
-                        }
-
-                        @Override
-                        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-                        }
-
-                        @Override
-                        public void onChildRemoved(DataSnapshot dataSnapshot) {
-                        }
-
-                        @Override
-                        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-                        }
-
-                        @Override
-                        public void onCancelled(DatabaseError databaseError) {
-                        }
-                    });
-                } else {
-                    Log.d(TAG, "onAuthStateChanged:signed_out");
-                }
-            }
-        };
 
         mRecyclerView = (RecyclerView) findViewById(R.id.list_view);
 
@@ -96,17 +53,44 @@ public class NotesActivity extends AppCompatActivity {
         mRecyclerView.setAdapter(mAdapter);
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
+    private void updateNotes(FirebaseUser user) {
+        userRef = mDatabase.getReference(user.getUid());
+        userRef.child("notes").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Note note = dataSnapshot.getValue(Note.class);
+                String key = dataSnapshot.getKey();
+                if (!keys.contains(key)) {
+                    keys.add(key);
+                    notes.add(note);
+                    mAdapter.notifyItemInserted(notes.size() - 1);
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
     }
 
     @Override
-    public void onStop() {
-        super.onStop();
-        if (mAuthListener != null) {
-            mAuth.removeAuthStateListener(mAuthListener);
+    public void onStart() {
+        super.onStart();
+        user = mAuth.getCurrentUser();
+        if (user != null) {
+            updateNotes(user);
         }
     }
 
